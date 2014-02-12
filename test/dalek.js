26aee2bd457285c98fd2f1a8b6c2df514d7f93d8
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-02-12 00:21:13 -0800

    the actual driver

---
+++ test/dalek.js
@@ -0,0 +1,39 @@
+module.exports = {
+	'signup username error': function ( test ) {
+		test
+		    .open('http://127.0.0.1:3030')
+		    .waitForElement('#password')
+		    .type('#email', 'jacoblowe2.0@gmail.com')
+		   	.type('#password', '123456')
+		   	.click('[type="submit"]')
+		   	.waitForElement('.error_message')
+		   	.assert.text('.error_message')
+		   		.is('username jacoblowe2.0@gmail.com already taken')
+		   	.done();	
+	},
+	'signup flow works': function (test) {
+
+		// good for now but there also should
+		// be a cleanup script
+		// and also test for errors in between
+
+		test
+		    .open('http://127.0.0.1:3030')
+		    .waitForElement('#password')
+		    .type('#email', 'temp' + ( Math.random() * 100000 ) + '@gmail.com')
+		   	.type('#password', '123456')
+		   	.click('[type="submit"]')
+		   	.waitForElement('.pure-button.alert.submit-plan')
+		   	.click('.plan-list > li >input[type="radio"]')
+		   	.click('.submit-plan')
+		   	.waitForElement('input[type="submit"]')
+		   	.type('#card_number', '4242424242424242')
+		   	.type('#cvc', '123')
+		   	.type('#card_exp_month', '02')
+		   	.type('#card_exp_year', '20')
+		   	.click('input[type="submit"]')
+		   	.waitForElement('.message')
+		   	.assert.text('.message').is('Thanks for joining!')
+		    .done();
+		}
+};
