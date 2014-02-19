4953486e3a337ed6440b54be231f03d37c45e173
scottmotte <scott@scottmotte.com>
2014-02-18 16:21:01 -0800

    Rebuild templates

--- app/javascripts/templates.js
+++ app/javascripts/templates.js
@@ -66,7 +66,7 @@ function program3(depth0,data) {
 function program5(depth0,data) {
   
   
-  return "\n		<h3>You have no plan</h3>\n		<p><a data-view=\"updateplan\">Choose a Plan</a></p>\n	";
+  return "\n		<h3>You have no plan</h3>\n		<p><a href=\"\" data-view=\"updateplan\">Choose a Plan</a></p>\n	";
   }
 
   buffer += "<div id=\"io-dashboard\">\n	<h2>Hello.</h2>\n	";
@@ -75,7 +75,7 @@ function program5(depth0,data) {
   buffer += "\n	";
   stack1 = helpers['if'].call(depth0, depth0.plan, {hash:{},inverse:self.program(5, program5, data),fn:self.program(3, program3, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\n	<p> What would you like to do?</p>\n	<ul>\n		<li> \n			<a data-view=\"donate\">Donate</a>\n		</li>\n		<li> \n			<a data-view=\"createevent\">Create A Event</a>\n		</li>\n		<li> \n			<a data-view=\"payment\">Make a Payment</a>\n		</li>\n		<li> \n			<a data-view=\"updateplan\">Update Plan</a>\n		</li>\n		<li> \n			<a data-view=\"updatepayment\">Change Card</a>\n		</li>\n		<li> \n			<a data-view=\"cancelplan\">Cancel Membership</a>\n		</li>\n		<li> \n			<a data-view=\"login\" onclick=\"app.logout()\">Logout</a>\n		</li>\n	</ul>\n\n</div>\n\n";
+  buffer += "\n	<p> What would you like to do?</p>\n	<ul>\n		<li> \n			<a href=\"\" data-view=\"donate\">Donate</a>\n		</li>\n		<li> \n			<a href=\"\" data-view=\"createevent\">Create A Event</a>\n		</li>\n		<li> \n			<a href=\"\" data-view=\"payment\">Make a Payment</a>\n		</li>\n		<li> \n			<a href=\"\" data-view=\"updateplan\">Update Plan</a>\n		</li>\n		<li> \n			<a href=\"\" data-view=\"updatepayment\">Change Card</a>\n		</li>\n		<li> \n			<a href=\"\" data-view=\"cancelplan\">Cancel Membership</a>\n		</li>\n		<li> \n			<a href=\"\" data-view=\"login\" onclick=\"app.logout()\">Logout</a>\n		</li>\n	</ul>\n\n</div>\n\n";
   return buffer;
   });
 
@@ -85,7 +85,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<h2>Donate to Riverside.io</h2>\n<p>Thanks you for your consideration.</p>\n<form class=\" pure-form pure-form-stacked\">\n	<label for=\"card_number\" data-fixture=\"$\">Amount</label>\n	<input type=\"text\" name=\"amount\" id=\"amount\" placeholder=\"0.00\" />\n	<label for=\"card_number\">Credit Card Number</label>\n	<input type=\"text\" name=\"card_number\" id=\"card_number\" />\n	<div class=\"pure-g-r\">\n		<div class=\"pure-u-1-3\">\n			<div>\n				<label for=\"cvc\">CVC</label>\n				<input type=\"text\" name=\"card_cvc\" id=\"cvc\" value=\"\"/ >\n			</div>\n		</div>\n		<div class=\"pure-u-1-3\">\n			<div>\n				<label for=\"card_exp_month\">Expiration Month</label>\n				<input type=\"text\" name=\"card_exp_month\" id=\"card_exp_month\" value=\"\"/ >\n			</div>\n		</div>\n		<div class=\"pure-u-1-3\">\n			<div>\n				<label for=\"card_exp_year\">Expiration Year</label>\n				<input type=\"text\" name=\"card_exp_year\" id=\"card_exp_year\" value=\"\"/ >\n			</div>\n		</div>\n	</div>\n	<input type=\"submit\" class=\"pure-button alert\" value=\"Submit\"/>\n	<a class=\"donate-cancel pure-button alert\">Cancel</a>\n</form>\n";
+  return "<h2>Donate</h2>\n<p>Thanks you for your consideration.</p>\n<form class=\" pure-form pure-form-stacked\">\n	<label for=\"card_number\" data-fixture=\"$\">Amount</label>\n	<input type=\"text\" name=\"amount\" id=\"amount\" placeholder=\"0.00\" />\n	<label for=\"card_number\">Credit Card Number</label>\n	<input type=\"text\" name=\"card_number\" id=\"card_number\" />\n	<div class=\"pure-g-r\">\n		<div class=\"pure-u-1-3\">\n			<div>\n				<label for=\"cvc\">CVC</label>\n				<input type=\"text\" name=\"card_cvc\" id=\"cvc\" value=\"\"/ >\n			</div>\n		</div>\n		<div class=\"pure-u-1-3\">\n			<div>\n				<label for=\"card_exp_month\">Expiration Month</label>\n				<input type=\"text\" name=\"card_exp_month\" id=\"card_exp_month\" value=\"\"/ >\n			</div>\n		</div>\n		<div class=\"pure-u-1-3\">\n			<div>\n				<label for=\"card_exp_year\">Expiration Year</label>\n				<input type=\"text\" name=\"card_exp_year\" id=\"card_exp_year\" value=\"\"/ >\n			</div>\n		</div>\n	</div>\n	<input type=\"submit\" class=\"pure-button alert\" value=\"Submit\"/>\n	<a class=\"donate-cancel pure-button alert\">Cancel</a>\n</form>\n";
   });
 
 this["__tmp"]["forgotpassword"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
